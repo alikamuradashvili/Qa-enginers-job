4077d28a341c2519591c42847bd052f0ac351164
alika muradashvili <am@invoo.io>
2025-11-13 12:10:12 +0400

    test

---
+++ qa-blog/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import angular from '@analogjs/vite-plugin-angular';
+
+export default defineConfig({
+  plugins: [angular()],
+  server: {
+    host: '0.0.0.0',
+    port: 4200,
+    allowedHosts: [
+      'all',
+      'localhost',
+      '192.168.88.75',
+      '.ngrok-free.dev'
+    ],
+    cors: true,
+  },
+});
